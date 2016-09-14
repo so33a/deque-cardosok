3c487678e66afcf90e29614a4939e0f393f0de82
cardosok <cesarcardoso954@gmail.com>
2016-09-14 20:12:24 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include "fila.h"
+#include "fila.c"
 
 int main () {
   FILA alunos = novaFila();
@@ -13,10 +13,15 @@ int main () {
   while(!filaVazia(alunos))
     printf ("removido: %d \n", remover(alunos));
 
-
+  inserirEsq(alunos, 7);
+  inserir(alunos, 8);
   inserir(alunos, 10);
+  inserirEsq(alunos, 3);
   inserir(alunos, 32);
-  
+  inserirEsq(alunos, 9);
+
+  printf("removendo ultimo\n", removerDir(alunos));
+
   imprimirFila(alunos);
 
   destroiFila(alunos);
